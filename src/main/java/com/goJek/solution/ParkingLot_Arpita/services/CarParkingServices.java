18ccea094954b3bdac61895728d737dce01813be
sharma-arpita <arpita625@gmail.com>
2017-04-01 08:13:25 +0530

    Initial model Completed.

---
+++ src/main/java/com/goJek/solution/ParkingLot_Arpita/services/CarParkingServices.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.goJek.solution.ParkingLot_Arpita.services;
+
+import java.util.List;
+
+/**
+ * @author arpita
+ *
+ */
+public class CarParkingServices extends ParkingLotServices {
+
+	/* (non-Javadoc)
+	 * @see com.goJek.solution.ParkingLot_Arpita.services.IParkingLotServices#park(java.lang.String, java.lang.String)
+	 */
+	public int park(String registrationNumber, String color) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.goJek.solution.ParkingLot_Arpita.services.IParkingLotServices#registration_numbers_for_cars_with_colour(java.lang.String)
+	 */
+	public List<String> registration_numbers_for_cars_with_colour(String color) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.goJek.solution.ParkingLot_Arpita.services.IParkingLotServices#slot_numbers_for_cars_with_colour(java.lang.String)
+	 */
+	public List<Integer> slot_numbers_for_cars_with_colour(String color) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
