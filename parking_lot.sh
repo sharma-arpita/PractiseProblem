c6e7982cbad2f227ea1d43fbfd1bfb933c8ba59b
sharma-arpita <arpita625@gmail.com>
2017-04-02 08:19:58 +0530

    Script file modified

--- parking_lot.sh
+++ parking_lot.sh
@@ -9,4 +9,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "Building Parking lot"
 mvn clean package
 echo "ENTER INPUT TO EXECUTE THE PROJECT:"
-eval "java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1044 -jar $DIR/target/ParkingLot_Arpita-0.0.1-SNAPSHOT-jar-with-dependencies.jar $1"
+eval "java $JAVA_OPTS -jar $DIR/target/ParkingLot_Arpita-0.0.1-SNAPSHOT-jar-with-dependencies.jar $1"
